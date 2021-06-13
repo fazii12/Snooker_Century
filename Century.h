7b552e6576d9c0eff477bc0d2ba6eb98a3e00c33
Faizan Tanveer <50085217+fazii12@users.noreply.github.com>
2021-06-13 14:13:25 +0500

    Add files via upload

---
+++ Century.h
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <string>
+#include <random>
+#include <time.h>
+#include <ctime>
+#include <iomanip>
+using namespace std;
+
+
+class Player
+{
+	string fname;
+	string lname;
+	int id;
+	int score;
+	int prev_score;
+	int strikes_taken;
+	int age;
+	int bestbreak;
+	int worstbreak;
+	clock_t start_time;
+	Player *next, *prev;
+	void allot_id();
+	bool finished;
+
+public:
+	
+	Player();
+	void set_name(string, string);
+	void set_prev(Player*);
+	void set_next(Player*);
+	void set_isfinished(bool);
+	void set_bestbreak(int);
+	void set_worstbreak(int);
+	bool get_finish_status();
+	Player* get_prev();
+	Player* get_next();
+	void update_scores(int);
+	string get_fullname();
+	void info();
+	void input_info();
+	int getscore();
+	int getstrikes();
+	void set_start_time();
+	int get_id();
+	int getbestbreak();
+	int getworstbreak();
+};
+
+
+class Player_list
+{
+	Player* front;
+	int no_of_players;
+
+public:
+	
+	Player_list();
+	void add_new_Player(Player*);
+	void del_Player(Player*&);
+	~Player_list();
+	int get_no_of_players();
+	Player* get_front();
+
+	void operator=(const Player_list&);
+};
+
+
+
+class Game
+{
+	friend void Sort(Game&);
+	int game_id;
+	Player_list players;
+	Player *players_array;
+	Player* winners_list;
+	
+	
+public:
+	
+	Game();
+	void list_to_arr();
+	void show_Scorecard();
+	void play();
+	void show_final_results(int);
+	
+	
+};
+
+
+
+
+
---
+++ driver.cpp
@@ -0,0 +1,32 @@
+#include "Century.h"
+
+int main() {
+
+	int choice , players;
+
+	try {
+		cout << "Choose from following\n";
+		cout << "\t\t\t\t1. Start Game\n";
+		cout << "\t\t\t\t2. Save Game\n";
+		cout << "\t\t\t\t<press any key to exit>\n";
+
+		cout << "\n\nYour choice <1 - 3>\t:\t";
+		cin >> choice;
+
+		
+
+		if (choice == 1) {
+			system("cls");
+			cout << "---------------------------------------------In Game--------------------------------------------------\n";
+
+
+			Game G;
+
+			G.play();
+		}
+	}
+	catch (exception ex) {
+		cout << ex.what() << endl;
+	}
+	
+}
